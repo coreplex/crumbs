6069008d56c50457fa122ccfdcd39187e3cba46d
Matthew Collison <matthew@sixteenstudio.com>
2015-03-16 10:50:21 +0000

    Add preparation methods

--- src/Crumbs.php
+++ src/Crumbs.php
@@ -1,5 +1,7 @@
 <?php namespace Coreplex\Crumbs;
 
+use Closure;
+
 class Crumbs {
 
     /**
@@ -17,6 +19,47 @@ class Crumbs {
     public function construct()
     {
         $this->crumbs = new Illuminate\Support\Collection;
+    }
+
+    /**
+     * Add an anonymous function to prepare the breadcrumbs
+     * 
+     * @param  Closure $closure
+     * @return $this
+     */
+    public function prepare(Closure $closure)
+    {
+        $this->addPreparation($closure);
+
+        return $this;
+    }
+
+    protected function build()
+    {
+        foreach ($this->getPreparations() as $preparation) {
+            $preparation($this);
+        }
+    }
+
+    /**
+     * Add a preparation closure to the crumbs instance
+     * 
+     * @param  Closure $closure
+     * @return void
+     */
+    protected function addPreparation(Closure $closure)
+    {
+        $this->preparations[] = $closure;
+    }
+
+    /**
+     * Retrieve all preparation closures from the crumbs instance
+     * 
+     * @return array
+     */
+    protected function getPreparations()
+    {
+        return $this->preparations;
     }
 
 }

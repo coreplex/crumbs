b9f082b7d4418481b1a2d8a5f4734c6eddecbba6
Matthew Collison <matthew@sixteenstudio.com>
2015-03-16 08:39:53 +0000

    Add collection to class

--- src/Crumbs.php
+++ src/Crumbs.php
@@ -1,3 +1,22 @@
 <?php namespace Coreplex\Crumbs;
 
-class Crumbs {}
+class Crumbs {
+
+    /**
+     * The collection of breadcrumbs
+     * 
+     * @var Illuminate\Support\Collection $crumbs
+     */
+    protected $crumbs;
+
+    /**
+     * Make a new crumbs instance
+     * 
+     * @return void
+     */
+    public function construct()
+    {
+        $this->crumbs = new Illuminate\Support\Collection;
+    }
+
+}

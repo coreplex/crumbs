890773141ddaf5b7c1d32e2448189f1e25e32943
Matthew Collison <matthew@sixteenstudio.com>
2015-03-17 15:10:05 +0000

    Add Laravel service provider and config

---
+++ config/crumbs.php
@@ -0,0 +1,18 @@
+<?php return array(
+
+	/**
+	 * The crumb implementation
+	 */
+	'crumb' => 'Coreplex\Crumbs\Components\Crumb',
+
+	/**
+	 * The renderer implementation
+	 */
+	'renderer' => 'Coreplex\Crumbs\Renderers\Renderer',
+
+	/**
+	 * The container implementation
+	 */
+	'container' => 'Coreplex\Crumbs\Container',
+
+);
---
+++ src/CrumbsServiceProvider.php
@@ -0,0 +1,108 @@
+<?php namespace Coreplex\Crumbs;
+
+use Coreplex\Crumbs\Container;
+use Coreplex\Crumbs\Components\Crumb;
+use Coreplex\Crumbs\Renderer\Basic as BasicRenderer;
+use Coreplex\Crumbs\Contracts\Container as ContainerContract;
+use Coreplex\Crumbs\Contracts\Renderer as RendererContract;
+use Coreplex\Crumbs\Contracts\Crumb as CrumbContract;
+use Illuminate\Support\ServiceProvider;
+
+class CrumbsServiceProvider extends ServiceProvider {
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Boot the service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('coreplex/crumbs', 'coreplex/crumbs');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->publishConfig();
+        $this->registerRenderer();
+        $this->registerCrumb();
+        $this->registerContainer();
+    }
+
+    /**
+     * Publishes the config
+     * 
+     * @return void
+     */
+    public function publishConfig()
+    {
+        $this->publishes([
+            __DIR__.'/config/crumbs.php' => config_path('crumbs.php'),
+        ]);
+    }
+
+    /**
+     * Register the renderer used by Crumbs.
+     * 
+     * @return void
+     */
+    public function registerRenderer()
+    {
+        $this->app['Coreplex\Crumbs\Contracts\Renderer'] = $this->app->share(function($app)
+        {
+            return new $app['config']->get('crumbs.renderer');
+        });
+    }
+
+    /**
+     * Register the crumb class used by Crumbs.
+     * 
+     * @return void
+     */
+    public function registerCrumb()
+    {
+        $this->app['Coreplex\Crumbs\Contracts\Crumb'] = $this->app->share(function($app)
+        {
+            return new $app['config']->get('crumbs.crumb');
+        });
+    }
+
+    /**
+     * Take the components of Crumbs together and build the Container
+     * 
+     * @return void
+     */
+    public function registerContainer()
+    {
+        $this->app['Coreplex\Crumbs\Contracts\Container'] = $this->app->share(function($app)
+        {
+            return new $app['config']->get('crumbs.container')($app['Coreplex\Crumbs\Contracts\Crumb'], $app['Coreplex\Crumbs\Contracts\Renderer']);
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [
+            'Coreplex\Crumbs\Contracts\Container',
+            'Coreplex\Crumbs\Contracts\Crumb',
+            'Coreplex\Crumbs\Contracts\Renderer',
+        ];
+    }
+
+}

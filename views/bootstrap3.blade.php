9da523069c9f84fbf75e63470afd68163c3c26ec
Matthew Collison <matthew@sixteenstudio.com>
2015-03-19 22:10:05 +0000

    Echo unescaped labels

--- views/bootstrap3.blade.php
+++ views/bootstrap3.blade.php
@@ -3,9 +3,9 @@
     @foreach($container->getCrumbs() as $crumb)
         <li @if($crumb->isCurrent()) class="active" @endif >
             @if( ! $crumb->isCurrent() && $crumb->hasUrl())
-                <a href="{{ $crumb->getUrl() }}">{{ $crumb->hasLabel() ? $crumb->getLabel() : $crumb->getUrl() }}</a>
+                <a href="{{ $crumb->getUrl() }}">{!! $crumb->hasLabel() ? $crumb->getLabel() : $crumb->getUrl() !!}</a>
             @else
-                {{ $crumb->hasLabel() ? $crumb->getLabel() : $crumb->getUrl() }}
+                {!! $crumb->hasLabel() ? $crumb->getLabel() : $crumb->getUrl() !!}
             @endif
         </li>
     @endforeach

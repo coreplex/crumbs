83a5364bcc5b871042ce1c8bed951c154dfc160d
knash94 <kylenash94@gmail.com>
2017-08-25 11:52:38 +0100

    Add schema data to breadcrumbs
    Add itemtypes and schema to comply with https://developers.google.com/search/docs/data-types/breadcrumbs

--- views/bootstrap3.blade.php
+++ views/bootstrap3.blade.php
@@ -1,12 +1,14 @@
 @if($container->count() > 0)
-<ol class="breadcrumb">
-    @foreach($container->getCrumbs() as $crumb)
-        <li @if($crumb->isCurrent()) class="active" @endif >
+<ol class="breadcrumb" itemscope itemtype="http://schema.org/BreadcrumbList">
+    @foreach($container->getCrumbs() as $key=>$crumb)
+        <li @if($crumb->isCurrent()) class="active" @endif itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
             @if( ! $crumb->isCurrent() && $crumb->hasUrl())
-                <a href="{{ $crumb->getUrl() }}">{!! $crumb->hasLabel() ? $crumb->getLabel() : $crumb->getUrl() !!}</a>
+                <a itemscope itemtype="http://schema.org/Thing"
+                   itemprop="item" href="{{ $crumb->getUrl() }}"><span itemprop="name">{!! $crumb->hasLabel() ? $crumb->getLabel() : $crumb->getUrl() !!}</span></a>
             @else
-                {!! $crumb->hasLabel() ? $crumb->getLabel() : $crumb->getUrl() !!}
+                <span itemprop="name">{!! $crumb->hasLabel() ? $crumb->getLabel() : $crumb->getUrl() !!}</span>
             @endif
+                <meta itemprop="position" content="{{ $key + 1 }}" />
         </li>
     @endforeach
 </ol>
